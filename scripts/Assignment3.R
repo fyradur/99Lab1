24e925d6dc1b7d6a4239c716c62da96423c344e0
John Möller <john.moller@outlook.com>
2024-11-25 05:32:31 +0100

    Decision boundary

--- scripts/Assignment3.R
+++ scripts/Assignment3.R
@@ -41,18 +41,23 @@ df |> ggplot() +
       geom_function(fun = plasma_function) +
       labs(x = "Age", y = "Plasma glucose concentration", color = "Predicted to have diabetes?")
 
-classification_threshold <- 0.2
-df$prediction02 <- df$predicted_prob > classification_threshold
+p <- 0.2
+df$prediction02 <- df$predicted_prob > p
+k4 <- log (p / (1-p))
+plasma_function <- function(x)( k4 -k1 -k2 * x) / k3
 
 df |> ggplot() +
       geom_point(aes(x = age, y = plasma, color = prediction02)) +
-      #geom_function(fun = plasma_function) +
+      geom_function(fun = plasma_function) +
       labs(x = "Age", y = "Plasma glucose concentration", color = "Predicted to have diabetes?")
 
-classification_threshold <- 0.8
-df$prediction08 <- df$predicted_prob > classification_threshold
+p <- 0.8
+df$prediction02 <- df$predicted_prob > p
+k4 <- log (p / (1-p))
+plasma_function <- function(x)( k4 -k1 -k2 * x) / k3
+
 
 df |> ggplot() +
       geom_point(aes(x = age, y = plasma, color = prediction08)) +
-      #geom_function(fun = plasma_function) +
+      geom_function(fun = plasma_function) +
       labs(x = "Age", y = "Plasma glucose concentration", color = "Predicted to have diabetes?")
