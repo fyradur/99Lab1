2a21ff35c9fba9bd2716fc1935fec4f8d28d0945
John Möller <john.moller@outlook.com>
2024-11-25 06:13:16 +0100

    New model

--- scripts/Assignment3.R
+++ scripts/Assignment3.R
@@ -52,7 +52,7 @@ df |> ggplot() +
       labs(x = "Age", y = "Plasma glucose concentration", color = "Predicted to have diabetes?")
 
 p <- 0.8
-df$prediction02 <- df$predicted_prob > p
+df$prediction08 <- df$predicted_prob > p
 k4 <- log (p / (1-p))
 plasma_function <- function(x)( k4 -k1 -k2 * x) / k3
 
@@ -61,3 +61,31 @@ df |> ggplot() +
       geom_point(aes(x = age, y = plasma, color = prediction08)) +
       geom_function(fun = plasma_function) +
       labs(x = "Age", y = "Plasma glucose concentration", color = "Predicted to have diabetes?")
+
+
+df$z1 <- df$plasma ^ 4
+df$z2 <- df$plasma ^ 3 * df$age
+df$z3 <- df$plasma ^ 2 * df$age ^ 2
+df$z4 <- df$plasma  * df$age ^ 3
+df$z5 <- df$age ^ 4
+
+m2 = glm(diabetes ~ age + plasma + z1 + z2 + z3 + z4 + z5,
+         data=df, family="binomial")
+
+df$predicted_prob2 <- predict(m2, newdata = df, type = "response")
+classification_threshold <- 0.5
+df$prediction2 <- df$predicted_prob2 > classification_threshold
+
+# 0 = k1 + k2 * age + k3 * plasma
+k1 <- summary(m)$coefficients[1]
+k2 <- summary(m)$coefficients[2]
+k3 <- summary(m)$coefficients[3]
+
+# plasma = (-k1 -k2 * age) / k3
+plasma_function <- function(x)(-k1 -k2 * x) / k3
+
+df |> ggplot() +
+      geom_point(aes(x = age, y = plasma, color = prediction)) +
+      geom_function(fun = plasma_function) +
+      labs(x = "Age", y = "Plasma glucose concentration", color = "Predicted to have diabetes?")
+
